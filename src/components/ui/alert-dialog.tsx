329d8bf4b3fc08e9c539472261ac73986bb34015
Sooraj Anilkumar <sooraj.anilkumar@twinswheel.fr>
2025-09-19 13:00:55 +0000

    fix the build error BuildError@https://9000-firebase-studio-175811044191

--- src/components/ui/alert-dialog.tsx
+++ src/components/ui/alert-dialog.tsx
@@ -2,7 +2,6 @@
 
 import * as React from "react"
 import * as AlertDialogPrimitive from "@radix-ui/react-alert-dialog"
-import { Users } from 'lucide-react';
 
 import { cn } from "@/lib/utils"
 import { buttonVariants } from "@/components/ui/button"
@@ -139,5 +138,4 @@ export {
   AlertDialogDescription,
   AlertDialogAction,
   AlertDialogCancel,
-  Users
 }
